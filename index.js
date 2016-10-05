19247e37fbd54960f3e27cc75c00e094e7a6ae90
adamcadaver <labi722@gmail.com>
2016-10-05 15:55:21 +0000

    added html page

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(app.get('port'), function() {
